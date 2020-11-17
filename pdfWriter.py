6eae8f2e0978ed0db99fd7a1171a643f59658e4a
DoctorLugubrious <averyg@utahcounty.gov>
2020-11-17 14:28:09 -0700

    simplify pdfWriter

--- pdfWriter.py
+++ pdfWriter.py
@@ -7,146 +7,157 @@ from imageProcessing.split import splitFile
 from imageProcessing.shrinkImage import shrinkImage, getImageDims
 
 
-
 def escapeString(toEscape):
-	return toEscape.translate(
-		str.maketrans({
-			"\\": "\\\\",
-			"^": "\\^",
-			"$": "\\$",
-			"*": "\\*",
-			"%": "\\%",
-			"{": "\\{",
-			"}": "\\}",
-			"#": "",
-			"&": "\\&",
-			"_": "\\_",
-			"|": "\\|"
-			}))
+    return toEscape.translate(
+        str.maketrans({
+            "\\": "\\\\",
+            "^": "\\^",
+            "$": "\\$",
+            "*": "\\*",
+            "%": "\\%",
+            "{": "\\{",
+            "}": "\\}",
+            "#": "",
+            "&": "\\&",
+            "_": "\\_",
+            "|": "\\|"
+        }))
 
 
 MARGIN = 20
+INDEX_FILENAME = ".index"
+BODY_FILE = "body.txt"
+HEADER_FILE = "header.txt"
+
+MAX_WIDTH = 1.3
+
 
 class pdfWriter:
-	def __init__(self, name, author, pageColor, textColor, optionalHeight, optionalWidth, jpgQuality):
-		self.title = name
+    def __init__(self, name, author, pageColor, textColor, optionalHeight, optionalWidth, jpgQuality):
+        self.title = name
 
+        self.pageHeight = int(optionalHeight) if optionalHeight else 1200
+        self.pageWidth = int(optionalWidth) if optionalWidth else 800
+        self.workWidth = self.pageWidth - (2 * MARGIN)
+        self.workHeight = self.pageHeight - (2 * MARGIN)
 
-		self.pageHeight = int(optionalHeight) if optionalHeight else 1200
-		self.pageWidth = int(optionalWidth) if optionalWidth else 800
-		self.jpgQuality = jpgQuality if jpgQuality else 95
+        self.jpgQuality = jpgQuality if jpgQuality else 95
+        self.pageColor = pageColor
 
-		self.workWidth = self.pageWidth - (2 * MARGIN)
-		self.workHeight = self.pageHeight - (2 * MARGIN)
-		self.comicNumber = 0
+        self.lastChapterName = ""
+        self.comicNumber = 0
+        self._restoreState()
 
-		self.lastChapterName = ""
+        generateHeader(name, author, pageColor, textColor,
+                       self.pageWidth, self.pageHeight, MARGIN)
 
-		self.bodyFile = "body.txt"
-		self.headerFile = "header.txt"
+        if not os.path.exists('images'):
+            os.makedirs('images')
 
-		self.pageColor = pageColor
+    # PUBLIC
+    def addComic(self, comic):
+        for image in comic.imageFiles:
+            self._addComic(comic, image)
 
-		self.addTitle = True
+    def save(self):
+        indexFile = open(INDEX_FILENAME, 'w')
+        indexFile.write(str(self.comicNumber) + "\n")
+        indexFile.write(self.lastChapterName)
+        indexFile.close()
 
-		self.indexFilename = ".index"
-		if(os.path.isfile(self.indexFilename)):
-			indexFile = open(self.indexFilename, 'r')
-			self.comicNumber = int(indexFile.readline())
-			self.lastChapterName = indexFile.readline()
-			indexFile.close()
+    def finish(self):
+        finalFilename = 'result.latex'
+        with open(finalFilename, 'w') as finalFile:
+            for f in [HEADER_FILE, BODY_FILE]:
+                with open(f, 'r') as currentFile:
+                    shutil.copyfileobj(currentFile, finalFile)
+            finalFile.write("\\end{document}")
 
-		generateHeader(name, author, pageColor, textColor, self.pageWidth, self.pageHeight, MARGIN)
+        subprocess.run(["rubber", "-d", finalFilename])
 
-		if not os.path.exists('images'):
-			os.makedirs('images')
+        os.remove("result.latex")
+        os.remove("result.aux")
+        os.remove("result.log")
+        if self.lastChapterName != "":
+            os.remove("result.toc")
 
-	def addComic(self, comic):
-		for image in comic.imageFiles:
-			self._addComic(comic, image)
-	
-	def _stripUnicode(self, string):
-		return escapeString(string.encode('ascii', "xmlcharrefreplace").decode('ascii'))
-	
-	def _getTitle(self, comic):
-		return self._stripUnicode(comic.title) if comic.title else ""
+    # PRIVATE
+    def _restoreState(self):
+        if(os.path.isfile(INDEX_FILENAME)):
+            indexFile = open(INDEX_FILENAME, 'r')
+            self.comicNumber = int(indexFile.readline())
+            self.lastChapterName = indexFile.readline()
+            indexFile.close()
 
-	def _getMouseover(self, comic):
-		return self._stripUnicode(comic.titleText)
+    def _stripUnicode(self, string):
+        return escapeString(string.encode('ascii', "replace").decode('ascii'))
 
-	def _addComic(self, comic, image):
-		self.comicNumber += 1
-		width, height = getImageDims(image)
-		widthFactor = 1.3
+    def _getTitle(self, comic):
+        return self._stripUnicode(comic.title) if comic.title else ""
 
-		self._addChapterName(comic)
+    def _getMouseover(self, comic):
+        return self._stripUnicode(comic.titleText)
 
-		if width >= self.workWidth * widthFactor and height < self.workHeight:
-			images = splitFile(image, self.workWidth, axis=1)
-			for i in range(len(images)):
-				self._addComicFullWidth(height, comic, images[i], suffix="h" + str(i))
-		else:
-			self._addComicFullWidth(height, comic, image)
+    def _addComic(self, comic, image):
+        self.comicNumber += 1
+        width, height = getImageDims(image)
 
-	def _addComicFullWidth(self, height, comic, image, suffix=""):
-		if height < self.workHeight:
-			image = self._getComicImage(image, suffix)
-			self._addComicInfo(self._getTitle(comic), image, self._getMouseover(comic))
-		else:
-			splitHeight = min(1, getImageDims(image)[0] / self.workWidth) * (self.workHeight + MARGIN)
-			images = splitFile(image, splitHeight)
-			for i in range(len(images)):
-				image = self._getComicImage(images[i], suffix="p" + str(i))
-				self._addComicInfo(self._getTitle(comic) if i == 0 else "",
-					image,
-					self._getMouseover(comic) if i is len(images) - 1 else "")
-				os.remove(images[i])
-	
-	def _addChapterName(self, comic):
-		name = escapeString(comic.chapterName)
-		if name != "" and name != self.lastChapterName:
-			if self.comicNumber == 1:
-				with open(self.bodyFile,'a') as comics:
-					comics.write("\t\\begingroup\\let\\cleardoublepage\\clearpage\\tableofcontents\\endgroup\n")
-		 
-			self.lastChapterName = name
+        self._addChapterName(comic)
 
-			with open(self.bodyFile,'a') as comics:
-				comics.write("""\t\\newpage
+        if width >= self.workWidth * MAX_WIDTH and height < self.workHeight:
+            images = splitFile(image, self.workWidth, axis=1)
+            for i in range(len(images)):
+                self._addComicFullWidth(comic, images[i], "h" + str(i))
+        else:
+            self._addComicFullWidth(comic, image)
+
+    def _addComicFullWidth(self, comic, image, suffix=""):
+        height = getImageDims(image)[1]
+        title = self._getTitle(comic)
+        mouseover = self._getMouseover(comic)
+
+        if height < self.workHeight:
+            image = self._getComicImage(image, suffix)
+            self._addComicInfo(title, image, mouseover)
+        else:
+            shrinkFactor = min(1, height / self.workWidth)
+            splitHeight = shrinkFactor * (self.workHeight + MARGIN)
+            images = splitFile(image, splitHeight)
+            self._addSplitImages(images, title, mouseover, suffix)
+
+    def _addSplitImages(self, images, title, mouseover, suffix=""):
+        for i in range(len(images)):
+            _suffix = suffix + "p" + str(i)
+            image = self._getComicImage(images[i], suffix)
+            _title = title if i == 0 else ""
+            _mouseover = mouseover if i is len(images) - 1 else ""
+            self._addComicInfo(_title, image, _mouseover)
+            os.remove(images[i])
+
+    def _addChapterName(self, comic):
+        name = escapeString(comic.chapterName)
+
+        if name != "" and name != self.lastChapterName:
+            with open(BODY_FILE, 'a') as comics:
+                if self.comicNumber == 1:
+                    comics.write(
+                        "\t\\begingroup\\let\\cleardoublepage\\clearpage\\tableofcontents\\endgroup\n")
+
+                comics.write("""\t\\newpage
 					\\begin{{center}}
 						\\vspace*{{\\fill}}
 						\\section{{{0}}}
 						\\vspace*{{\\fill}}
 					\\end{{center}}
 					\\newpage\n""".format(name))
-			
 
-	def finish(self):
-		finalFilename = 'result.latex'
-		with open(finalFilename,'w') as finalFile:
-			for f in [self.headerFile, self.bodyFile]:
-				with open(f,'r') as currentFile:
-					shutil.copyfileobj(currentFile, finalFile)
-			finalFile.write("\\end{document}")
+            self.lastChapterName = name
 
-		subprocess.run(["rubber", "-d", finalFilename])
+    def _getComicImage(self, comic, suffix=""):
+        title = ''.join(e for e in self.title if e.isalnum())
+        return shrinkImage(comic, self.workWidth, title, self.comicNumber, self.pageColor, self.jpgQuality, suffix)
 
-		os.remove("result.latex")
-		os.remove("result.aux")
-		os.remove("result.log")
-		if self.lastChapterName != "":
-			os.remove("result.toc")
-
-	def _getComicImage(self, comic, suffix=""):
-		title = ''.join(e for e in self.title if e.isalnum())
-		return shrinkImage(comic, self.workWidth, title, self.comicNumber, self.pageColor, self.jpgQuality, suffix)
-	
-	def _addComicInfo(self, title, image, mouseover):
-		with open(self.bodyFile,'a') as comics:
-			comics.write("\t\\comic{{{0}}}{{{1}}}{{{2}}}\n".format(title, image, mouseover))
-
-	def save(self):
-		indexFile = open(self.indexFilename, 'w')
-		indexFile.write(str(self.comicNumber) + "\n") 
-		indexFile.write(self.lastChapterName) 
-		indexFile.close()
+    def _addComicInfo(self, title, image, mouseover):
+        with open(BODY_FILE, 'a') as comics:
+            comics.write("\t\\comic{{{0}}}{{{1}}}{{{2}}}\n".format(
+                title, image, mouseover))
